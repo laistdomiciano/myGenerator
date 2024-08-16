fdc3dbd3b119fb62bb9a62c61c8691d9d125ea64
laistdomiciano <laistdomiciano@gmail.com>
2024-08-16 10:02:43 +0200

    try to build database

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,4 +1,4 @@
-# from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-#
-# def create_jwt_token(user_id):
-#     return create_access_token(identity=user_id)
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+def create_jwt_token(user_id):
+    return create_access_token(identity=user_id)
--- backend/app/run.py
+++ backend/app/run.py
@@ -1,4 +1,4 @@
-from app.init import myapp
+from init import myapp
 
 if __name__ == "__main__":
     myapp.run(debug=True)
