a8181a6d1d25dfc5f8f4e618aace36d69976af2a
laistdomiciano <laistdomiciano@gmail.com>
2024-08-16 12:54:23 +0200

    work

--- backend/app/init.py
+++ backend/app/init.py
@@ -11,24 +11,22 @@ jwt = JWTManager()
 
 
 def create_database(myapp):
-    if not os.path.exists(DB_NAME):
-        with myapp.app_context():
-            db.create_all()
-        print('Created Database!')
+    with myapp.app_context():
+        db.create_all()
+    print('Created Database!')
 
 
 def create_app():
     myapp = Flask(__name__)
-    # myapp.config['SECRET_KEY'] = 'mysupersecretkey'
-    # myapp.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://lais.domiciano@hotmail.com:2206@localhost/{DB_NAME}'
+
     myapp.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'mysupersecretkey')
-    myapp.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',
-                                                             'postgresql://lais.domiciano@hotmail.com:2206@localhost/mygenerator.db')
+    myapp.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            'DATABASE_URL',
+            'postgresql://lais.domiciano@hotmail.com:2206@localhost:5432/mygenerator')
 
     myapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(myapp)
-
     migrate.init_app(myapp, db)
     jwt.init_app(myapp)
 
