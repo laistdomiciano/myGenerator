749e26eee6baa4c85a4bce7c60953e937afffa28
laistdomiciano <laistdomiciano@gmail.com>
2024-08-22 13:32:10 +0200

    auth.py

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,4 +1,17 @@
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import jwt
+import datetime
+from flask import current_app
 
 def create_jwt_token(user_id):
-    return create_access_token(identity=user_id)
+    token = jwt.encode({
+        'user_id': user_id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)
+    }, current_app.config['SECRET_KEY'], algorithm='HS256')
+    return token
+
+def decode_jwt_token(token):
+    try:
+        payload = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
+        return payload['user_id']
+    except jwt.ExpiredSignatureError:
+        return None
